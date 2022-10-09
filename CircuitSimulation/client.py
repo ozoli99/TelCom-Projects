29b120ec3285235ee408ebd60f12fb013fe783e1
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-10-09 19:31:21 +0200

    Refactoring

--- CircuitSimulation/client.py
+++ CircuitSimulation/client.py
@@ -1,5 +1,33 @@
 import json
 
+def reserve_relevant_links():
+    global event_successful
+
+    # Iterate through the points in the route
+    for j in range(0, len(circuit) - 1):
+        # Reserve the relevant link in the route
+        for link in links:
+            if link["points"][0] == circuit[j] and link["points"][1] == circuit[j + 1]:
+                if link["reserved"] == False and link["capacity"] >= demand["demand"]:
+                    link["reserved"] = True
+                    event_successful = True
+                    demand["switches"].append(link["points"][1])
+                    link["capacity"] = link["capacity"] - demand["demand"]
+                    break
+                else:
+                    event_successful = False
+                    break
+
+def release_relevant_links():
+    for j in range(0, len(demand["switches"]) - 1):                
+        for link in links:
+            if link["points"][0] == demand["switches"][0] and link["points"][1] == demand["switches"][1]:
+                link["reserved"] = False
+                link["capacity"] = link["capacity"] + demand["demand"]
+                demand["switches"].remove(demand["switches"][0])
+                break
+    demand["switches"].remove(demand["switches"][0])
+
 with open("e:\ELTE\V. Felev\Telekommunikacios halozatok\Beadandok\TelCom-Projects\CircuitSimulation\cs1.json", "r") as read_file:
     data = json.load(read_file)
     
@@ -28,25 +56,14 @@ for i in range(0, simulation["duration"] + 1):
             for circuit in possible_circuits:    
                 # If there's a route between the end points
                 if circuit[0] == demand["end-points"][0] and circuit[len(circuit) - 1] == demand["end-points"][1]:
-                    # Iterate through the points in the route
-                    for j in range(0, len(circuit) - 1):
-                        # Reserve the relevant link in the route
-                        for link in links:
-                            if link["points"][0] == circuit[j] and link["points"][1] == circuit[j + 1]:
-                                if link["reserved"] == False and link["capacity"] >= demand["demand"]:
-                                    link["reserved"] = True
-                                    event_successful = True
-                                    demand["switches"].append(link["points"][1])
-                                    link["capacity"] = link["capacity"] - demand["demand"]
-                                    break
-                                else:
-                                    event_successful = False
-                                    break
+                    reserve_relevant_links()
                     break
+
             if event_successful:
                 print(str(event_number) + ". " + event_name + ": " + event_endpoints + " st:" + str(event_start_time) + " - sikeres")
             else:
                 print(str(event_number) + ". " + event_name + ": " + event_endpoints + " st:" + str(event_start_time) + " - sikertelen")
+            
             number = number + 1
         elif demand["end-time"] == i + 1:
             event_number = number
@@ -54,14 +71,8 @@ for i in range(0, simulation["duration"] + 1):
             event_end_time = demand["end-time"]     
             event_name = "igény felszabadítás"
 
-            for j in range(0, len(demand["switches"]) - 1):                
-                for link in links:
-                    if link["points"][0] == demand["switches"][0] and link["points"][1] == demand["switches"][1]:
-                        link["reserved"] = False
-                        link["capacity"] = link["capacity"] + demand["demand"]
-                        demand["switches"].remove(demand["switches"][0])
-                        break
-            demand["switches"].remove(demand["switches"][0])
+            release_relevant_links()
 
             print(str(event_number) + ". " + event_name + ": " + event_endpoints + " st:" + str(event_start_time))
+            
             number = number + 1
